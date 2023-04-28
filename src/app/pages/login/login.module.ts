210a3bf834f696963e3c4c67c3326e20a22d31a6
haventrooper <diego.riveros.g@mail.pucv.cl>
2023-04-28 13:38:55 -0400

    actualizacion rama dr 28 abril

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PrimengModule } from '../../modules/primeng.module';  //Importar componentes de Primeng
-import { LoginComponent } from './login.component';            //Importar componente login.ts de pages/login
-import { LoginRoutingModule } from './login-routing.module';   //Importar componente login.module.ts de pages/login
+import { PrimengModule } from '../../modules/primeng.module';  
+import { LoginComponent } from './login.component';            
+import { LoginRoutingModule } from './login-routing.module';   
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
-//Se importan para tenerlos declarados previamente y no tener problemas de compilacion ya que no existen sin las librerias
+
+
+
+
+
 @NgModule({
 	declarations: [LoginComponent],
-	imports: [CommonModule, PrimengModule, LoginRoutingModule, FormsModule],
+	imports: [CommonModule, PrimengModule, LoginRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class LoginModule {}
