49c738ec8ae559e9f273ff87df3230fadd944c15
Viztrew <vicente.moraga.puyo@gmail.com>
2023-05-01 20:23:08 -0400

    Inicialización página signup

--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SignupComponent } from './signup.component';
+import { PrimengModule } from 'src/app/modules/primeng.module';
+import { SignupRoutingModule } from './signup-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+	declarations: [SignupComponent],
+	imports: [CommonModule, PrimengModule, SignupRoutingModule],
 })
-export class SignupModule { }
+export class SignupModule {}
